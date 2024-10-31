36019831955843d7a61bedd389ddf9b26f4de421
Seongil Yoon <books13456@gmail.com>
2024-10-31 15:12:53 +0900

    add: oauth2 url path

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
 from accounts.views import HelloView
+from accounts.provider import FortyTwoProvider
+from allauth.socialaccount.providers.oauth2.urls import default_urlpatterns
 
 urlpatterns = [
     path('', include('dj_rest_auth.urls')),
     path('registration/', include('dj_rest_auth.registration.urls')),
     path('hello/', HelloView.as_view(), name='hello'),
+    path('oauth2/', include(default_urlpatterns(FortyTwoProvider))),
 ]
