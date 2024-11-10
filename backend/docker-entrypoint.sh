4a84d271d6bc397f98024f0894e51e8cf5f774fc
minsepar <parkj12b@github.com>
2024-11-11 01:02:06 +0900

    fix: dockerfile 빌드 오류 수정

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/bin/sh
+
+pip install -r requirements.txt
 
 python manage.py makemigrations
 python manage.py migrate
-
-exec python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000
