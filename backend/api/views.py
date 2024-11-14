9175b372bf56c7d260de3e4296668c53d6a85596
박민서 <ppms03@naver.com>
2024-11-15 03:19:20 +0900

    refact: remove unused test view

--- backend/api/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django_otp.decorators import otp_required
-
-@otp_required
-def test(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
