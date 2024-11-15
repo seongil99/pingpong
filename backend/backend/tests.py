deff04c5db9d17c90e030869a4b9cb55f7fde310
minsepar <parkj12b@github.com>
2024-11-15 20:34:21 +0900

    feat: reorganize tests and move spaLoginRequiredView to separate module

--- backend/backend/tests.py
+++ backend/backend/tests.py
@@ -1,17 +1,9 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from .decorators import spa_login_required
 from django.test import TestCase
 from rest_framework.test import APIRequestFactory, force_authenticate
 from django.contrib.auth import get_user_model
 from rest_framework import status
 from django.urls import reverse
-
-@api_view(['GET'])
-@spa_login_required
-def spaLoginRequiredView(request):
-    return Response({'message': 'Success'}, status = 200)
-
+from tests.views import spaLoginRequiredView
 
 User = get_user_model()
 
