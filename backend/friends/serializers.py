c026f754f4f27b88aa389288706e1d52e63fd111
minsepar <parkj12b@github.com>
2024-11-20 20:25:11 +0900

    fix: merge with develop before push

--- backend/friends/serializers.py
+++ backend/friends/serializers.py
@@ -1,6 +1,8 @@
 from django.core.validators import MinValueValidator
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from drf_spectacular.utils import extend_schema_field
+from accounts.users.serializers import UserProfileSerializer
 from .models import Friend
 
 User = get_user_model()
@@ -18,18 +20,13 @@ class FriendSerializer(serializers.ModelSerializer):
         fields = ["id", "user1", "user2", "requester", "status", "created_at"]
 
 
-from drf_spectacular.utils import extend_schema_field, OpenApiTypes
-from accounts.users.serializers import UserSerializer
-from .models import Friend
-
-
 class FriendRequestWithOtherUserSerializer(serializers.ModelSerializer):
     """
     Serializer for the friend request list that includes the 'other_user' annotation.
     """
 
     other_user = serializers.SerializerMethodField()
-    
+
     class Meta:
         model = Friend  # Replace with your actual FriendRequest model
         fields = ("id", "requester", "status", "created_at", 'other_user')
@@ -41,13 +38,12 @@ class FriendRequestWithOtherUserSerializer(serializers.ModelSerializer):
             "other_user",
         )
 
-    @extend_schema_field(UserSerializer)
+    @extend_schema_field(UserProfileSerializer)
     def get_other_user(self, obj):
         """
         This method returns the User instance for the other_user.
         """
-        
-        if obj.user1 == self.context["request"].user:
-            return UserSerializer(obj.user2).data
-        return UserSerializer(obj.user1).data
 
+        if obj.user1 == self.context["request"].user:
+            return UserProfileSerializer(obj.user2).data
+        return UserProfileSerializer(obj.user1).data
