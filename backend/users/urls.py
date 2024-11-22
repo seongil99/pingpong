b2cce164e0fc90dec9d85be7e465c1a162777a0e
minsepar <parkj12b@github.com>
2024-11-22 19:25:43 +0900

    .

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -22,10 +22,10 @@ blocked_user_detail = UserBlockedViewset.as_view(
 
 
 urlpatterns = [
+    path("", UserSearchView.as_view(), name="user-search"),
     path("accounts/", include("users.accounts.urls")),
     path("me/", MyProfileView.as_view(), name="my-profile"),
     path("friends/", include("users.friends.urls")),
-    path("", UserSearchView.as_view(), name="user-search"),
     path(
         "friendable/",
         SearchFriendableView.as_view(),
