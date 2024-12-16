5f9564ced1e04354585207d66ff9fc6160a6fce0
Seongil Yoon <books13456@gmail.com>
2024-12-16 13:37:36 +0900

    feat: add docs

--- backend/users/accounts/views.py
+++ backend/users/accounts/views.py
@@ -2,7 +2,7 @@ from rest_framework import permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from dj_rest_auth.jwt_auth import JWTCookieAuthentication
-from drf_spectacular.utils import extend_schema
+from drf_spectacular.utils import extend_schema, OpenApiExample
 
 import logging
 
@@ -19,11 +19,38 @@ class VerifyView(APIView):
         return Response(content, status=200)
 
 
-@extend_schema(tags=["users"])
+@extend_schema(
+    tags=["users"],
+    examples=[
+        OpenApiExample(
+            "Anonymous User Response",
+            value={
+                "is_logged_in": False,
+                "status": "anonymous",
+                "message": "user is not logged in",
+            },
+            response_only=True,
+            status_codes=["200"],
+        ),
+        OpenApiExample(
+            "Authenticated User Response",
+            value={
+                "is_logged_in": True,
+                "status": "logged in",
+                "message": "user is logged in",
+            },
+            response_only=True,
+            status_codes=["200"],
+        ),
+    ],
+)
 class CheckAnonymousView(APIView):
     authentication_classes = [JWTCookieAuthentication]
     permission_classes = [permissions.AllowAny]
 
+    @extend_schema(
+        responses={"200": {"description": "로그인 상태 확인"}},
+    )
     def get(self, request):
         if not request.user.is_authenticated:
             content = {
