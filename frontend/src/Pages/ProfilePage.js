01a96a2ca5b6f5d89a28b92cd9816ac259df8704
jhdgo1225 <parkjh99365@gmail.com>
2024-12-20 20:28:47 +0900

    import statuschecker function and stat description of profile page are resolved

--- frontend/src/Pages/ProfilePage.js
+++ frontend/src/Pages/ProfilePage.js
@@ -61,12 +61,15 @@ class ProfilePage {
             if (userHistoryData.results[idx].winner === userid) gameWinCount++;
             const start = new Date(userHistoryData.results[idx].started_at);
             const end = new Date(userHistoryData.results[idx].ended_at);
-            const diff = end.getTime() - start.getTime();
+            const diff =
+                userHistoryData.results[idx].ended_at !== null
+                    ? end.getTime() - start.getTime()
+                    : 0;
             playtime += diff;
             if (longestRallyCount < userHistoryData.results[idx].longest_rally)
                 longestRallyCount = userHistoryData.results[idx].longest_rally;
             averageRallyTotal += userHistoryData.results[idx].average_rally;
-            (userHistoryData.results[idx].tournament_id)
+            userHistoryData.results[idx].tournament_id
                 ? (mode["tournament"] += 1)
                 : (mode["pvp"] += 1);
         }
@@ -493,6 +496,7 @@ class ProfilePage {
                         const pagination = document.querySelector(
                             "#history-description-pagination"
                         );
+                        console.log(pagination);
                         pagination.classList.remove("hide");
                         const description = document.querySelector(
                             "#profile-stat-or-history-description"
@@ -578,7 +582,7 @@ class ProfilePage {
                 user.id
             );
         }
-        const pagination = this.#Pagination(userHistoryData, info, user.id);
+        const pagination = await this.#Pagination(userHistoryData, info, user.id);
         const description = createElement(
             "div",
             {
