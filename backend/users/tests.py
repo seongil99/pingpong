0d61a1840e6f0085aeda4785897f58adf0c1ac7f
Seongil Yoon <books13456@gmail.com>
2024-12-21 14:20:50 +0900

    fix: test code

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -64,7 +64,7 @@ class MyProfileTestCase(APITestCase):
             avatar=None,
         )
         data = {
-            'username': 'test',
+            'username': 'test1',
         }
         response = self.client.patch(self.url, data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
